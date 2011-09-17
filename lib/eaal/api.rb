fa964d1755b16d10bccabeb167a6db1804a2ea9f
Marcus Rückert <darix@nordisch.org>
2011-09-17 23:36:25 +0200

    - whitespace cleanup

--- lib/eaal/api.rb
+++ lib/eaal/api.rb
@@ -8,7 +8,7 @@
 #  }
 class EAAL::API
   attr_accessor :userid, :key, :scope
-      
+
   # constructor
   # Expects:
   # * userid (String | Integer) the users id
@@ -19,19 +19,19 @@ class EAAL::API
     self.key = key.to_s
     self.scope = scope.to_s
   end
-  
+
   # create an xml request according to the method called
-  # this is used to dynamicaly create api calls and 
+  # this is used to dynamicaly create api calls and
   # should usually not be called directly
-  # * method (const) 
-  # * args 
+  # * method (const)
+  # * args
   def method_missing(method, *args)
     scope = self.scope
     args_hash = args.first
     args_hash = {} unless args_hash
     self.request_xml(scope, method.id2name, args_hash)
   end
-    
+
   # make a request to the api. will use cache if set.
   # usually not called by the user directly
   # * scope (String)
@@ -43,16 +43,16 @@ class EAAL::API
     if not xml
       source = URI.parse(EAAL.api_base + scope + '/' + name +'.xml.aspx')
       req_path = source.path + format_url_request(opts.merge({
-        :userid => self.userid, 
+        :userid => self.userid,
         :apikey => self.key}))
       req = Net::HTTP::Get.new(req_path)
       req[EAAL.version_string]
-      res = Net::HTTP.new(source.host, source.port).start {|http| http.request(req) } #one request for now  
+      res = Net::HTTP.new(source.host, source.port).start {|http| http.request(req) } #one request for now
       case res
       when Net::HTTPOK
       when Net::HTTPNotFound
         raise EAAL::Exception::APINotFoundError.new("The requested API (#{scope} / #{name}) could not be found.")
-      else 
+      else
         raise EAAL::Exception::HTTPError.new("An HTTP Error occured, body: " + res.body)
       end
       EAAL.cache.save(self.userid, self.key, scope,name,opts, res.body)
@@ -61,7 +61,7 @@ class EAAL::API
     doc = Hpricot.XML(xml)
     result = EAAL::Result.new(scope.capitalize + name, doc)
   end
-              
+
   # Turns a hash into ?var=baz&bam=boo
   # stolen from Reve (thx lisa)
   # * opts (Hash)
